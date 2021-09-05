aa846ff5e544d37316322005c63f0f8e57660a9e
Yun-SeYeong <dbstpdud09@naver.com>
2021-09-05 22:23:07 +0900

    [UPDATE] update

---
+++ src/main/java/japbook/jpashap/JpashapApplication.java
@@ -0,0 +1,13 @@
+package japbook.jpashap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/japbook/jpashap/JpashapApplicationTests.java
@@ -0,0 +1,13 @@
+package japbook.jpashap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
